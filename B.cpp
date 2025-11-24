9b939b744d3293296b7baf1ed09d6546d571b9d7
nicccce <lin2832856512@163.com>
2025-11-24 23:00:08 +0800

    buti

---
+++ B.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+int mod = 1e9+7;
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+    struct cube{int x,y,z,idx,idy,idz;};
+    auto solve = [&]() {
+        int l,w,h,p,q,r,n;
+        cin>>l>>w>>h>>p>>q>>r>>n;
+        vector<cube> a(n);
+        for(int i=0;i<n;i++)cin>>a[i].x>>a[i].y>>a[i].z;
+        int ans=1,cnt=0,ind=0;
+        if(n%((p+1)*(q+1)*(r+1)))ans=0;
+        sort(a.begin(),a.end(),[&](cube x,cube y){
+            return x.x<y.x;
+        });
+        for(int i=1;i<n;i++){
+            if(i%(n/(p+1))==0)ans=(ans*(a[i].x-a[i-1].x))%mod,cnt++;
+            a[i].idx=cnt;
+        }sort(a.begin(),a.end(),[&](cube x,cube y){
+            return x.y<y.y;
+        });
+        vector<int>c(p+1);
+        cnt=0;
+        for(int i=0;i<n;i++){
+            c[a[i].idx]++;
+            if(c[a[i].idx]==(ind+1)*(n/(p+1)/(q+1))+1){
+                if(cnt!=p+1)ans=0;
+                else cnt=0,ind+=1,ans=(ans*(a[i].y-a[i-1].y))%mod;
+            }if(c[a[i].idx]==(ind+1)*(n/(p+1)/(q+1)))cnt++;
+            a[i].idy=ind;
+        }sort(a.begin(),a.end(),[&](cube x,cube y){
+            return x.z<y.z;
+        });
+        c=vector<int>(p+1);
+        cnt=0,ind=0;
+        for(int i=0;i<n;i++){
+            c[a[i].idx]++;
+            if(c[a[i].idx]==(ind+1)*(n/(p+1)/(r+1))+1){
+                if(cnt!=p+1)ans=0;
+                else cnt=0,ind+=1,ans=(ans*(a[i].z-a[i-1].z))%mod;
+            }if(c[a[i].idx]==(ind+1)*(n/(p+1)/(r+1)))cnt++;
+            a[i].idz=ind;
+        }sort(a.begin(),a.end(),[&](cube x,cube y){
+            return array<int,3>{x.idx,x.idy,x.idz}<array<int,3>{y.idx,y.idy,y.idz};
+        });cnt=1;
+        for(int i=1;i<n;i++){
+            if(a[i].idz!=a[i-1].idz){
+                if(cnt!=n/((p+1)*(q+1)*(r+1)))ans=0;
+                cnt=0;
+            }cnt++;
+        }
+        cout<<ans<<'\n';
+    };
+    solve();
+    return 0;
+}
---
+++ warmC.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, k;
+vector<int> a, pre;
+
+void solve() {
+    cin >> n >> m >> k;
+    a.assign(n + 1, 0);
+    pre.assign(n + 1, 0);
+    
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    
+    sort(a.begin() + 1, a.end());
+    
+    for (int i = 1; i <= n; i++) {
+        pre[i] = pre[i - 1] + a[i];
+    }
+    
+    int x = upper_bound(a.begin() + 1, a.end(), m) - a.begin();
+    x--;
+    
+    long long mi = 1e14;
+    for (int y = 1; y <= k - x; y++) {
+        long long cnt = pre[x] * y + pre[k - y] * y - (pre[k] - pre[k - y]) * (n - k) + m * (n - x + y) * y;
+        mi = min(mi, cnt);
+    }
+
+    long long basic = 0;
+    for (int i = 1; i <= k; i++) {
+        basic += a[i] * (n - i + 1);
+    }
+
+    mi = min(mi + basic, basic);
+    cout << mi << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
