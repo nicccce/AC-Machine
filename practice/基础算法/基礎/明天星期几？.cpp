bba34b2163ea0a0b2d3b50ec8e5a253aca73164e
nicccce <lin2832856512@163.com>
2025-12-28 09:01:16 +0800

    666

---
+++ practice/基础算法/基礎/明天星期几？.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+
+    auto solve = [&]() {
+        int d;
+        cin>>d;
+        cout<<(d)%7+1;
+    };
+    // int t;
+    // for(cin>>t;t;t--)
+    solve();
+    return 0;
+}
---
+++ practice/基础算法/基礎/温标转换.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+
+    auto solve = [&]() {
+        double k;cin>>k;
+        cout<<(k-273.15)*1.8+32;
+    };
+    // int t;
+    // for(cin>>t;t;t--)
+    solve();
+    return 0;
+}
