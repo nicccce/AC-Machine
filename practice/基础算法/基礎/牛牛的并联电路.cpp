0901a3b5d868c4e45cff873f25b9e376edcc2fca
nicccce <lin2832856512@163.com>
2025-12-28 11:00:42 +0800

    666

---
+++ practice/基础算法/基礎/牛牛的并联电路.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+
+    auto solve = [&]() {
+        int n;
+        cout<<n*1024*1024/4;
+    };
+    // int t;
+    // for(cin>>t;t;t--)
+    solve();
+    return 0;
+}
