0cef35e44624f7d9fd7dde5459132f6d81449a66
nicccce <lin2832856512@163.com>
2026-01-13 15:32:44 +0800

    hello 2026

---
+++ contests/codeforces/Hello 2026/A.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/contest/2183/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+const int N = 1e5, mod = 998244353;
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    auto solve = [&]() {
+    };
+    int t;
+    for (cin >> t; t--;)
+        solve();
+    return 0;
+}
