47da8dc79de7ed0a2dd5911dd5334362517fb913
nicccce <lin2832856512@163.com>
2026-01-14 17:47:29 +0800

    hello 2026

--- contests/codeforces/Hello 2026/A.cpp
+++ contests/codeforces/Hello 2026/A.cpp
@@ -9,6 +9,18 @@ signed main()
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     auto solve = [&]() {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        if(a[0]==1 || a[n-1]==1){
+            cout << "Alice\n";
+            return;
+        }else{
+            cout << "Bob\n";
+            return;
+        }
     };
     int t;
     for (cin >> t; t--;)
