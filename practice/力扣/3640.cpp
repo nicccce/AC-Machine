25e38587ed794e99ea2b5fd561bbe4630ce61a90
nicccce <lin2832856512@163.com>
2026-02-05 20:20:06 +0800

    leetcode

---
+++ practice/力扣/3640.cpp
@@ -0,0 +1,81 @@
+// https://leetcode.cn/problems/trionic-array-ii/?envType=daily-question&envId=2026-02-04
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+class Solution {
+public:
+    long long maxSumTrionic(vector<int>& nums) {
+        int n=nums.size();
+        vector<ll> prefix(n), suffix(n);
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                prefix[i]=max(prefix[i-1]+nums[i],(ll)nums[i]);
+            }else{
+                prefix[i]=nums[i];
+            }
+            cout<<prefix[i]<<" ";
+        }
+        cout<<endl;
+        suffix[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                suffix[i]=max(suffix[i+1]+nums[i],(ll)nums[i]);
+            }else{
+                suffix[i]=nums[i];
+            }
+            cout<<suffix[i]<<" ";
+        }
+        cout<<endl;
+        ll ans=LONG_LONG_MIN,sum=prefix[0];
+        for(int i=1;i<n-1;i++){
+            if(nums[i]<nums[i-1]){
+                sum=max(sum+nums[i],nums[i]+prefix[i-1]);
+            }else{                                                                                                                                                                                                                                          
+                sum=nums[i]+prefix[i-1];
+            }
+            ans=max(ans,sum+suffix[i+1]);
+        }
+        return ans;
+    }
+};
+
+
+ll qpow(ll b, ll p, ll mod)
+{
+    ll r = 1;
+    while (p)
+    {
+        if (p & 1)
+            r = r * b % mod;
+        b = b * b % mod, p >>= 1;
+    }
+    return r;
+}
+int ex_gcd(int a, int b, int &x, int &y)
+{
+    if (!b)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    else
+    {
+        int d = ex_gcd(b, a % b, y, x);
+        y -= a / b * x;
+        return d;
+    }
+}
+
+int solve_linear_congruence_equation(int a, int b, int n)
+{
+    int x, y;
+    int d = ex_gcd(a, n, x, y);
+    if (b % d)
+        return -1;
+    n /= d;
+    return ((long long)x * (b / d) % n + n) % n;
+}
