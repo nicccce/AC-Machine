e72dbb9337bbcd2e6181c18df62582fe5d020bab
nicccce <lin2832856512@163.com>
2026-03-09 12:34:02 +0800

    codeforces

--- contests/vp/HIT2026春季选拔赛第一场/C.cpp
+++ contests/vp/HIT2026春季选拔赛第一场/C.cpp
@@ -1,50 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int mod=998244353;
-int qpow(int b, int p){
-    int r = 1;
-    while (p){
-        if (p & 1)
-            r = r * b % mod;
-        b = b * b % mod, p >>= 1;
-    }
-    return r;
-}
-int inv(int x){
-    return qpow(x, mod - 2);
-}
+
 signed main() {
-    // ios::sync_with_stdio(0);
-    // cin.tie(0),cout.tie(0);
-    vector<int> jc(200005);
-    jc[1]=jc[0]=1;
-    for(int i=2;i<200000;i++){
-        jc[i]=jc[i-1]*i%mod;
-    }
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+
     auto solve = [&]() {
         int n;
         cin>>n;
-        vector<bool> a(n+1);
-        int cnt0=0,cnt1=0;
-        for(int i=1;i<=n;i++){
-            char c;
-            cin>>c;
-            a[i]=(c=='1');
+        vector<int> a(n+1);
+        for(int i=1;i<=n;i++)cin>>a[i];
+        vector<vector<int>> tree(n+1,vector<int>());
+        for(int i=1;i<n;i++){
+            int u,v;
+            cin>>u>>v;
+            tree[u].push_back(v);
+            tree[v].push_back(u);
         }
-        for(int i=1;i<=n;i++){
-            if(!a[i]){
-                cnt0++;
-            }else if(i<n&&a[i+1]){
-                cnt1++,i++;
+        vector<int> sum(n+1),dp(n+1);
+        function<void(int,int)> dfs=[&](int now,int father){
+            sum[now]=a[now];
+            for(auto i:tree[now]){
+                if(i==father)continue;
+                dfs(i,now);
+                // if(now==5){
+                //     cout<<i<<' '<<sum[i]<<'\n';
+                // }
+                sum[now]+=sum[i];
             }
+        };
+        dfs(1,0);
+        for(int i=2;i<=n;i++){
+            dp[1]+=sum[i];
         }
-        int ans=jc[cnt0+cnt1]*inv(jc[cnt1])%mod*inv(jc[cnt0])%mod;
+        int ans=dp[1];
+        function<void(int,int)> dfsdp=[&](int now,int father){
+            dp[now]=dp[father];
+            dp[now]+=sum[1]-2*sum[now];
+            ans=max(dp[now],ans);
+            for(auto i:tree[now]){
+                if(i==father)continue;
+                dfsdp(i,now);
+            }
+        };
+        // cout<<sum[1]<<'\n';
+        for(auto i:tree[1]){
+            dfsdp(i,1);
+        }
         cout<<ans<<'\n';
-        
     };
-    int t;
-    for(cin>>t;t;t--)
+    // int t;
+    // for(cin>>t;t;t--)
     solve();
     return 0;
 }
